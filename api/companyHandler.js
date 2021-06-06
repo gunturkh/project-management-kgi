fa9bc4eae405c7960252e6084cc5934199899952
dwihp2 <m.dwiheryanto2@gmail.com>
2021-06-06 16:02:20 +0700

    finishing CRUD for company API

--- api/companyHandler.js
+++ api/companyHandler.js
@@ -39,7 +39,39 @@ router.delete('/:id', auth, async (req, res, next) => {
     catch (error) {
         next(error)
     }
+})
 
+// update company content based on id
+router.patch('/:id', auth, async (req, res,next) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = [
+        'companyName',
+        'companyEmail',
+        'companyAddress',
+        'companyLogo',
+    ]
+    console.log('update?', updates)
+    const isValidOperation = updates.every((update) => {
+        console.log('update?', update)
+        return allowedUpdates.includes(update)
+    })
+    console.log('isValidOperation?', isValidOperation)
+    if (!isValidOperation)
+        return res.status(400).send({ error: 'Invalid updates!'})
+    try {
+        await Company.findOneAndUpdate(
+            {_id},
+            req.body,
+            { new:true, runValidators:true }
+        )
+        const company = await Company.find({})
+        if (!company) return res.status(404).send({error: 'Company not found!'})
+        res.send(company)
+    }
+    catch (error) {
+        next(error)
+    }
 })
 
 module.exports = router
